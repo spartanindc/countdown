788b88f17a0dbc609c34d6813fb114b926864373
David Alim <daveay@gmail.com>
2019-03-30 16:49:33 -0400

    initial commit to homepage flipper branch
    WIP to make this responsive...

--- public/clientside-profile.js
+++ public/clientside-profile.js
@@ -1,73 +1,233 @@
 //Handle Buttons and AJAX calls
 
-  //Keep the clock creation on the front end so that
-  //the time is set to the user's time zone.
+//Keep the clock creation on the front end so that
+//the time is set to the user's time zone.
 
-$(document).ready(function(){
-  if($('#countdowns-length').length){
-    var countdownLength = $('#countdowns-length').val();
-    for(var i=0; i<countdownLength; i++){
-      if((new Date($('#clock-'+i).attr('data-time-left')+"T00:00:00").getTime() / 1000) - (new Date().getTime()/1000) > 0) {
-        var clock = $('#clock-'+i)
-          .FlipClock((new Date($('#clock-'+i).attr('data-time-left')+"T00:00:00")
-                          .getTime() / 1000) - (new Date().getTime()/1000), {
-            clockFace: 'DailyCounter',
+$(document).ready(function() {
+  if ($("#countdowns-length").length) {
+    var countdownLength = $("#countdowns-length").val();
+    for (var i = 0; i < countdownLength; i++) {
+      if (
+        new Date(
+          $("#clock-" + i).attr("data-time-left") + "T00:00:00"
+        ).getTime() /
+          1000 -
+          new Date().getTime() / 1000 >
+        0
+      ) {
+        var clock = $("#clock-" + i).FlipClock(
+          new Date(
+            $("#clock-" + i).attr("data-time-left") + "T00:00:00"
+          ).getTime() /
+            1000 -
+            new Date().getTime() / 1000,
+          {
+            clockFace: "DailyCounter",
             countdown: true,
             autoPlay: true
-          });
+          }
+        );
       } else {
-        $('#clock-'+i).html('<p class="finished">Countdown has finished!</p>');
+        $("#clock-" + i).html(
+          '<p class="finished">Countdown has finished!</p>'
+        );
       }
     }
   }
 });
 
+// Set up flaps ////////////////////////////////////////////
+speed = 0.18; // seconds
+beginStr = "COUNTDOWN".toUpperCase().split("");
+endStr = "DDMMYYYY".toUpperCase().split("");
+// A-Z, 0-9, spaces only
+
+amountOfFlaps =
+  beginStr.length >= endStr.length ? beginStr.length : endStr.length;
+
+div = document.querySelector(".flipper");
+html = "";
+for (var x = 0; x < amountOfFlaps; x++) {
+  html +=
+    '<div class=splitflap><div class="top"></div><div class="bottom"></div><div class="nextHalf"></div><div class="nextFull"></div></div>';
+}
+
+div.innerHTML = html;
+
+// Set up more stuff ///////////////////////////////////////
+a1 = document.querySelectorAll(".top");
+a2 = document.querySelectorAll(".bottom");
+b1 = document.querySelectorAll(".nextFull");
+b2 = document.querySelectorAll(".nextHalf");
+
+for (var x = 0; x < a1.length; x++) {
+  a2[x].style.animationDuration = speed + "s";
+  b2[x].style.animationDuration = speed + "s";
+}
+
+// And even more ///////////////////////////////////////////
+char = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "0",
+  " ",
+  "-"
+];
+
+(strCount = []), (flag = []);
+
+for (var x = 0; x < amountOfFlaps; x++) {
+  if (beginStr.length != amountOfFlaps) {
+    for (var x = 0; x < amountOfFlaps - beginStr.length; x++) {
+      beginStr.push(" ");
+    }
+  } else if (endStr.length != amountOfFlaps) {
+    for (var x = 0; x < amountOfFlaps - endStr.length; x++) {
+      endStr.push(" ");
+    }
+  }
+}
+for (var x = 0; x < amountOfFlaps; x++) {
+  strCount[x] = char.indexOf(beginStr[x]);
+  (flag[x] = false), (flag2 = true);
+}
+
+// Flip them flaps /////////////////////////////////////////
+setInterval(function() {
+  for (var x = 0; x < amountOfFlaps; x++) {
+    if (b1[x].innerHTML == endStr[x]) dontFlipIt(x);
+    else flipIt(x);
+
+    if (
+      flag.every(function(e) {
+        return e;
+      }) &&
+      flag2
+    )
+      (flag2 = false), changeDestination();
+  }
+}, speed * 1000);
+
+////////////////////////////////////////////////////////////
+// Flap flipping functions /////////////////////////////////
+////////////////////////////////////////////////////////////
+function flipIt(x) {
+  a1[x].innerHTML = char[strCount[x] == 0 ? char.length - 1 : strCount[x] - 1];
+  a2[x].innerHTML = char[strCount[x] == 0 ? char.length - 1 : strCount[x] - 1];
+  b1[x].innerHTML = char[strCount[x]];
+  b2[x].innerHTML = char[strCount[x]];
+
+  a2[x].classList.remove("flip1");
+  a2[x].offsetWidth = a2[x].offsetWidth;
+  a2[x].classList.add("flip1");
+  b2[x].classList.remove("flip2");
+  b2[x].offsetWidth = b2[x].offsetWidth;
+  b2[x].classList.add("flip2");
+
+  if (strCount[x] > char.length - 2) strCount[x] = 0;
+  else strCount[x]++;
+}
+
+function dontFlipIt(x) {
+  flag[x] = true;
+  a2[x].classList.remove("flip2");
+  a2[x].style.backgroundColor = "#3BB6eB";
+  b2[x].style.backgroundColor = "#3BB6eB";
+  a1[x].innerHTML = char[strCount[x] == 0 ? char.length - 1 : strCount[x] - 1];
+  a2[x].innerHTML = char[strCount[x] == 0 ? char.length - 1 : strCount[x] - 1];
+}
+
+function changeDestination() {
+  setTimeout(function() {
+    flag.fill(false);
+    flag2 = true;
+
+    var tempArr = endStr.slice();
+    endStr = beginStr.slice();
+    beginStr = tempArr.slice();
+  }, 3000);
+}
 
 // Create
 function createButton() {
-
   //Create Form Display Toggle
-  $('.create').click(function(event) {
-    $('.create-countdown').slideToggle('slow');
+  $(".create").click(function(event) {
+    $(".create-countdown").slideToggle("slow");
     $(this).hide();
   });
 }
 
 function toggleUpdateButton() {
-
   //Update Form Display Toggle
-  $('.update').click(function(event) {
-    $(this).parent().children('.edit-countdown').slideToggle('slow');
+  $(".update").click(function(event) {
+    $(this)
+      .parent()
+      .children(".edit-countdown")
+      .slideToggle("slow");
     $(this).hide();
-    console.log('Update button clicked');
+    console.log("Update button clicked");
   });
 }
 
 // Delete
 
-  //Delete Button Event Handler
+//Delete Button Event Handler
 function deleteButton() {
-  $('.delete').click(function() {
+  $(".delete").click(function() {
     deleteCountdown(this);
   });
 }
 
-  //Delete AJAX call
+//Delete AJAX call
 function deleteCountdown(self) {
-
   let countdownID = $(self).val();
 
-  let prefix = '/countdowns/';
+  let prefix = "/countdowns/";
 
   $.ajax({
     url: prefix + countdownID,
-    method: 'DELETE',
-    contentType: 'application/json',
+    method: "DELETE",
+    contentType: "application/json",
     success: function(res) {
-      $(self).parent().remove();
+      $(self)
+        .parent()
+        .remove();
     },
     error: function(req) {
-      console.log('Delete Failed');
+      console.log("Delete Failed");
     }
   });
 }
--- server.js
+++ server.js
@@ -1,52 +1,54 @@
 //Tools
 
-const express  = require('express');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const flash    = require('connect-flash');
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const flash = require("connect-flash");
 
-const moment   = require('moment');
+const moment = require("moment");
 moment().format();
 
-const morgan       = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser   = require('body-parser');
-const session      = require('express-session');
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const session = require("express-session");
 
-const app  = express();
+const app = express();
 const jsonParser = bodyParser.json();
 const port = process.env.PORT || 8080;
 
 //Configuration
 
-const configDB = require('./config/database.js');
+const configDB = require("./config/database.js");
 mongoose.connect(configDB.url);
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 //Express Setup
 
-app.use(express.static('public'));
-app.use(morgan('common'));
+app.use(express.static("public"));
+app.use(morgan("common"));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.set('view engine', 'ejs');
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.set("view engine", "ejs");
 
 //Passport Setup
 
-app.use(session({ secret: 'operationcantwaitanylonger' }));
+app.use(session({ secret: "operationcantwaitanylonger" }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
 //Routes
-require('./app/routes.js')(app, passport);
+require("./app/routes.js")(app, passport);
 
 //Server Launch
 app.listen(port);
-console.log('Server is running at ' + port);
+console.log("Server is running at " + port);
 
 //exports
-module.exports = {app};
+module.exports = { app };
